75df7a47f9ba3f0a46b95f34758f4532d5c85796
EC2 Default User <ec2-user@ip-172-31-72-74.ec2.internal>
2022-04-16 06:49:06 +0000

    Tutorial Code Added in Environment Preperation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
-    system("python \"c:/Users/nikhi/Desktop/Email App/Hackathon/scripts.py\"");
+    cout << "Print this" << endl;
+    system("python3.9 my_scripts.py");
     return 0;
 }
